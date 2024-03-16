332163aa9b72fe8eeac117c7abcc01f11300936b
adarsh landge <adarsh.landge10604@gmail.com>
2024-03-17 00:41:24 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],        //Tailwind CSS har jagah use krna chahte hai
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
